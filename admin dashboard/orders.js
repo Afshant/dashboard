ee8f7cf77f16c8d5caf8099eea958d973a19a299
Afshan Tabassum <130816397+Afshant@users.noreply.github.com>
2023-12-01 08:17:12 +0530

    Add files via upload

---
+++ admin dashboard/orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {
+        productName: 'Foldable mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Larvender KF102 Drone',
+        productNumber: '36378',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Ruko F11 PPro Drone',
+        productNumber: '49347',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Drone with Camera',
+        productNumber: '96996',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'GPS 4K Drone',
+        productNumber: '22821',
+        paymentStatus: 'Paid',
+        shipping: 'Dekivered'
+    },
+    {
+        productName: 'DJL Air 25',
+        productNumber: '81475',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Lorenge Drone',
+        productNumber: '00482',
+        paymentStatus: 'Paid',
+        shipping: 'Dekivered'
+    },
+]
